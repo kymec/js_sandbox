d875c7a5c0ff30ba4453529bdea9178820487d50
Denis <kymec327@gmail.com>
2019-05-29 00:53:39 +0300

    eye

--- lesson_4/eye.js
+++ lesson_4/eye.js
@@ -1,22 +1,41 @@
 window.addEventListener('mousemove', (event) => {    
-    eye1.style.left = `calc(${beginX}% + ${calcCorrection(event).x}px)`;
-    eye1.style.top = `calc(${beginY}% + ${calcCorrection(event).y}px)`;
-    eye2.style.left = `calc(${beginX}% - 50px + ${calcCorrection(event).x}px)`;
-    eye2.style.top = `calc(${beginY}% + ${calcCorrection(event).y}px)`;    
+    eye1.style.left = `calc(${beginX}% - 5px + ${calcCorrection(event).x1}px)`;
+    eye1.style.top = `calc(${beginY}px + ${calcCorrection(event).y1}px)`;
+    eye2.style.left = `calc(${beginX}% - 58px + ${calcCorrection(event).x2}px)`;
+    eye2.style.top = `calc(${beginY}px + ${calcCorrection(event).y2}px)`;    
 });
+
+
+
+
 
 function calcCorrection(event){
     let width = document.getElementsByTagName("body")[0].clientWidth;
-    let height = heightWindow;
-    x = ((event.clientX - (width / 2)) / (width / 2)) * 5;
-    y = ((event.clientY - (height / 2)) / (height / 2)) * 5;
-    console.log(`x: ${x}; y: ${y}`);
-    return {"x": x, "y": y};
+    let a = beginY - event.clientY;
+    let b = beginX * (width / 100) - event.clientX;
+    angel1 = Math.atan(a/b);
+    angel2 = Math.atan(a/(b - 50));
+    y1 = zr * Math.sin(angel1);
+    y2 = zr * Math.sin(angel2);
+    x1 = zr * Math.cos(angel1);
+    x2 = zr * Math.cos(angel2);
+    if (b > 0){
+        x1 *= -1;
+        y1 *= -1;
+    }
+    if (b > 50){
+        x2 *= -1;
+        y2 *= -1;
+    }
+    
+    
+    return {"x1": x1, "y1": y1, "x2": x2, "y2": y2};
 }
 const heightWindow = 800;
 const diameterEyes = 7;
-const beginX = 49.5;
-const beginY = 36;
+const beginX = 50;
+const beginY = 237;
+const zr = 5;
 
 const eyes = document.createElement("div");
 document.body.append(eyes);
@@ -25,13 +44,13 @@ eyes.style.textAlign = "center";
 
 const mliza = document.createElement("img");
 mliza.src = "./mliza.jpg";
-mliza.style.margin = "0 auto";
+mliza.style.margin = "20px auto";
 eyes.append(mliza);
 
 const eye1 = document.createElement("div");
 eye1.style.position = "absolute";
-eye1.style.left = `calc(${beginX}%)`;
-eye1.style.top = `calc(${beginY}%)`;
+eye1.style.left = `calc(${beginX}% - 5px)`;
+eye1.style.top = `calc(${beginY}px)`;
 eye1.style.width = `${diameterEyes}px`;
 eye1.style.height = `${diameterEyes}px`;
 eye1.style.borderRadius = "50%";
@@ -40,8 +59,8 @@ eyes.append(eye1);
 
 const eye2 = document.createElement("div");
 eye2.style.position = "absolute";
-eye2.style.left = `calc(${beginX}% - 50px)`;
-eye2.style.top = `calc(${beginY}%)`;
+eye2.style.left = `calc(${beginX}% - 58px)`;
+eye2.style.top = `calc(${beginY}px)`;
 eye2.style.width = `${diameterEyes}px`;
 eye2.style.height = `${diameterEyes}px`;
 eye2.style.borderRadius = "50%";
