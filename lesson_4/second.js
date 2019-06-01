964f0e4cedd4bd56b4d7d8ef128f31197ee0766f
Denis <kymec327@gmail.com>
2019-06-01 14:05:40 +0300

    repair

--- lesson_4/second.js
+++ lesson_4/second.js
@@ -88,56 +88,54 @@ function calculate(){
         let result;
         do{
             result = prompt(`Введите 1, если необходимо произвести рассчёт формулы y = kx + b. \nВведите 2, если необходимо произвести рассчёт формулы y = x^2.\nВведите exit если необходимо закончить рассчёты`);
-        }
-        while(result !== "1" && result !== "2" && result !== "exit");
-        let y = 0;
-        if(result === "1" || result === "2" || result === "exit"){
             resObj.history.push(result);
         }
+        while(result !== "1" && result !== "2" && result !== "exit");
+        let y = 0;        
         if (result === "1"){
             resObj.formula = "y = kx + b";
             do{
               result = prompt(`Введите k для рассчёта формулы y = kx + b`);  
+              resObj.history.push(result);
             }            
             while((typeof(+result) !== "number" || isNaN(result) || result === null || result == "") && result !== "exit");            
-            resObj.history.push(result);
             if(result === "exit"){
                 return resObj;
             }
             resObj.args.k = +result;
             do{
                 result = prompt(`Введите x для рассчёта формулы y = kx + b`);
+                resObj.history.push(result);
             }
             while((typeof(+result) !== "number" || isNaN(result) || result === null || result == "") && result !== "exit");
-            resObj.history.push(result);
             if(result === "exit"){
                 return resObj;
             }            
             resObj.args.x = +result;  
             do{
                 result = prompt(`Введите b для рассчёта формулы y = kx + b`);
+                resObj.history.push(result);
             }
             while((typeof(+result) !== "number" || isNaN(result) || result === null || result == "") && result !== "exit");
-            resObj.history.push(result);
             if(result === "exit"){
                 return resObj;
             }            
             resObj.args.b = +result;
-            y = resObj.args.k * resObj.args.x + resObj.args.b;
+            y = parseFloat(resObj.args.k) * parseFloat(resObj.args.x) + parseFloat(resObj.args.b);
             alert(y);
             calc(resObj);
         }else if(result === "2"){
             resObj.formula = "y = x^2";
             do{
                 result = prompt(`Введите x для рассчёта формулы y = x^2`);
+                resObj.history.push(result);
             }
-            while((typeof(+result) !== "number" || isNaN(result) || result === null || result == "") && result !== "exit")            
-            resObj.history.push(result);
+            while((typeof(+result) !== "number" || isNaN(result) || result === null || result == "") && result !== "exit")           
             if(result === "exit"){
                 return resObj;
             }
             resObj.args.x = +result;
-            y = Math.pow(resObj.args.x, 2);
+            y = Math.pow(parseFloat(resObj.args.x), 2);
             alert(y);
             calc(resObj);
         }else if(result === "exit"){
